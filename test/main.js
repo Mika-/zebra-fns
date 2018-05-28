1a196e161c5010b6b662443ec7901b8f29a8b7c7
Mika <mika.simanainen@gmail.com>
2018-05-28 09:19:09 +0300

    add encoding tests

--- test/main.js
+++ test/main.js
@@ -18,5 +18,21 @@ describe('Generators', () => {
             expect(resolve('abcd')).to.equal(START_CODE_B);
             expect(resolve('1234')).to.equal(START_CODE_C);
         });
+
+        it('Test wikipedia example', () => {
+            expect(code128('PJJ123C')).to.equal('1101000010011101110110101101110001011011100010011100110110011100101100101110010001000110111010110001100011101011');
+        });
+
+        it('Test codeset A', () => {
+            expect(code128('ABCD')).to.equal('1101000010010100011000100010110001000100011010110001000110001000101100011101011');
+        });
+
+        it('Test codeset B', () => {
+            expect(code128('abcd')).to.equal('1101001000010010110000100100001101000010110010000100110110111011101100011101011');
+        });
+
+        it('Test codeset C', () => {
+            expect(code128('1234')).to.equal('110100111001011001110010001011000100100111101100011101011');
+        });
     });
 });
